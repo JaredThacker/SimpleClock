a111bec913b67376d0f8790daf948305b3e1d79a
Jared <jared@Zipcoders-MacBook-Pro-3.local>
2024-07-10 16:33:04 -0400

    added GMT n 24hr buttons

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,8 +2,11 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 
 public class SimpleClock extends JFrame {
@@ -12,6 +15,9 @@ public class SimpleClock extends JFrame {
         SimpleDateFormat timeFormat;
         SimpleDateFormat dayFormat;
         SimpleDateFormat dateFormat;
+        SimpleDateFormat timeFormatMilitary;
+        SimpleDateFormat timeFormatGMT;
+        Boolean isClickedOnce = false;
     
         JLabel timeLabel;
         JLabel dayLabel;
@@ -24,14 +30,17 @@ public class SimpleClock extends JFrame {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
             this.setLayout(new FlowLayout());
-            this.setSize(350, 220);
+            this.setSize(400, 230);
             this.setResizable(false);
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
+            timeFormatMilitary = new SimpleDateFormat("HH:mm:ss");
+            timeFormatGMT = new SimpleDateFormat("HH:mm:ss z");
+            timeFormatGMT.setTimeZone(TimeZone.getTimeZone("GMT"));
             dayFormat=new SimpleDateFormat("EEEE");
             dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
             timeLabel = new JLabel();
-            timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
+            timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 56));
             timeLabel.setBackground(Color.BLACK);
             timeLabel.setForeground(Color.WHITE);
             timeLabel.setOpaque(true);
@@ -40,18 +49,53 @@ public class SimpleClock extends JFrame {
     
             dateLabel=new JLabel();
             dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
-    
+
+            JButton militaryTime = new JButton("24HR");
+            militaryTime.setPreferredSize(new Dimension(100, 30));
+            militaryTime.setBackground(Color.black);
+            militaryTime.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(!isClickedOnce){
+                        timeFormat = timeFormatMilitary;
+                    } else {
+                        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+                    }
+                    isClickedOnce = !isClickedOnce;
+                }
+            });
+
+            JButton gmtTime = new JButton("GMT");
+            gmtTime.setPreferredSize(new Dimension(100, 30));
+            gmtTime.setBackground(Color.black);
+            gmtTime.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(!isClickedOnce){
+                        timeFormat = timeFormatGMT;
+                    } else {
+                        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+                    }
+                    isClickedOnce = !isClickedOnce;
+                }
+            });
+
+
+            this.add(gmtTime);
+            this.add(militaryTime);
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
             this.setVisible(true);
+
     
             setTimer();
         }
     
         public void setTimer() {
             while (true) {
+//                TimeZone gmtTime = TimeZone.getTimeZone("GMT");
+
                 time = timeFormat.format(Calendar.getInstance().getTime());
                 timeLabel.setText(time);
     
